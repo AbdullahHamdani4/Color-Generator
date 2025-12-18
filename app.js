f0a0c70aae46b0980166ab206c8f29189e5bc1dd
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-12-18 21:31:31 +0500

    fix: correct RGB display text and update button functionality

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ function change() {
     let b=Math.floor(Math.random() * 257)
     document.body.style.backgroundColor=`rgb(${r}, ${g}, ${b})`
     para.innerText=`Rgb code of the color is : rgb(${r}, ${g}, ${b})`
-    navigator.clipboard.writeText(`rgb(${r}, ${g}, ${b})`2)
+    // navigator.clipboard.writeText(`rgb(${r}, ${g}, ${b})`2)
 }
